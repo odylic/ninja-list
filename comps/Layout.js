d804dbd33cebdd9c3930ec4b217baed49f944215
Jimmy <odylic.lin@gmail.com>
2022-05-10 14:52:03 -0700

    initial

---
+++ comps/Layout.js
@@ -0,0 +1,14 @@
+import Navbar from "./navbar";
+import Footer from "./Footer";
+
+const Layout = ({ children }) => {
+  return (
+    <div className="content">
+      <Navbar />
+      {children}
+      <Footer />
+    </div>
+  );
+};
+
+export default Layout;
